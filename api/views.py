20d29422ee7b52beef653134702cf2c733c6198e
dreamxinxcode <dreamxinxcode@gmail.com>
2021-03-10 13:36:00 -0800

    fix requirements.txt

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
 
-# Create your views here.
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List': '/products/',
+    }
+    return Response(api_urls)
+
+
+@api_view(['GET'])
+def api_overview(request):
