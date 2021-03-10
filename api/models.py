576b84a3cd36f7599142ea44740fe0ac158555c9
dreamxinxcode <dreamxinxcode@gmail.com>
2021-03-10 13:21:09 -0800

    create product model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+  title = models.CharField(max_length=255, blank=True, null=True)
+  sku = models.CharField(max_length=255, blank=True, null=True)
+  price = models.CharField(max_length=255, blank=True, null=True)
+
+  def __srt__(self):
+    return self.title
+
