718b58c51047e8391d05c3c2c9da86c2d40e0d56
dreamxinxcode <dreamxinxcode@gmail.com>
2021-03-10 18:47:48 -0800

    fetching works

--- api/utils.py
+++ api/utils.py
@@ -2,10 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
+    'upgrade-insecure-requests': '1',
+    'dnt': '1'
+}
 
 
 def fetch_wayfair_price(url):
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, 'lxml')
-    print(soup)
+    with requests.Session() as session:
+        post = session.get(url, headers=headers)
+        soup = BeautifulSoup(post.text, 'html.parser')
+    print(soup.find("span", {"class": "notranslate"}).contents[0])
+
+
+fetch_wayfair_price(
+    'https://www.wayfair.ca/furniture/pdp/mistana-katarina-5-piece-extendable-solid-wood-dining-set-mitn2584.html')
